9f48df8c52d0be7059fee162a18ad9db8694b49d
OtavioAugustto <otavioaugustto93@gmail.com>
2021-07-22 15:50:09 -0300

    Create and Define interface off modal

--- alura-acessibility-2/src/app/app.component.ts
+++ alura-acessibility-2/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { ModalService, ModalRef } from './shared/components/modal/services/modal.service';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   title = 'a11y-p2';
+  public firstName = 'Otávio';
+  public ModalRef: ModalRef;
+  constructor(private modalservice: ModalService) {}
+
+  public show(): void {
+    this.ModalRef = this.modalservice.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details',
+    });
+  }
 }
--- alura-acessibility-2/src/app/shared/components/modal/services/modal.service.ts
+++ alura-acessibility-2/src/app/shared/components/modal/services/modal.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, TemplateRef } from '@angular/core';
 
 @Injectable()
-export class ModalService {}
+export class ModalService {
+  public open(config: ModalConfig): ModalRef {
+    console.log('open called');
+    return new ModalRef();
+  }
+}
+
+export interface ModalConfig {
+  templateRef: TemplateRef<any>;
+  title: string;
+}
+
+export class ModalRef{
+  public close(): void{
+    console.log('closed called');
+  }
+}
