182e6956e408c09b22a0bc67af8f08b0d3874d51
OtavioAugustto <otavioaugustto93@gmail.com>
2021-08-11 20:59:22 -0300

    Implementing Accessibility and Finishing the Project

--- alura-acessibility-2/src/app/app.component.ts
+++ alura-acessibility-2/src/app/app.component.ts
@@ -1,25 +1,47 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { invalid } from '@angular/compiler/src/render3/view/util';
+import { Component, TemplateRef, ViewChild, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalRef } from './shared/components/modal/models/modal-ref';
 import { ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   title = 'a11y-p2';
   public firstName = 'Otávio';
   public modalRef: ModalRef;
   public info = false;
+  public form: FormGroup;
+  constructor(
+    private modalService: ModalService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  constructor(private modalService: ModalService) {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['Otávio', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false],
+    });
+  }
 
   public show(): void {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
-      title: 'User Details'
+      title: 'User Details',
     });
+  }
+
+  public submit(): void {
+    if (this.form.invalid) {
+      return
+    }
+    console.log(this.form.value);
+    this.modalRef.close();
   }
 }
--- alura-acessibility-2/src/app/app.module.ts
+++ alura-acessibility-2/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from './shared/components/modal/modal.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     ModalModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
